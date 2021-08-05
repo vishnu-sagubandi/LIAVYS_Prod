0d8069c563a4e64809a6fd809ae7eb2a723eea8a
vishnu-sagubandi <sagubandi.vmnaidu.eee19@itbhu.ac.in>
2021-08-06 00:18:07 +0530

    Update

--- backend/settings.py
+++ backend/settings.py
@@ -123,11 +123,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("DB_NAME", ''),
-        'USER': os.environ.get("DB_USER", ''),
-        'PASSWORD': os.environ.get("DB_PASSWORD", ''),
-        'HOST': os.environ.get("DB_HOST", ''),
-        'PORT': os.environ.get("DB_PORT", '')
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT")
     }
 }
 
@@ -183,10 +183,10 @@ STATICFILES_DIRS = [
 
 # AWS Credentials
 
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", '')
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 # ''
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", '')
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", '')  # ''
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")  # ''
 
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
