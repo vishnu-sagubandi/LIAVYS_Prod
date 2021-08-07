5dda42ad24184557aaa68e8196a021fe762bcb2c
vishnu-sagubandi <sagubandi.vmnaidu.eee19@itbhu.ac.in>
2021-08-07 16:19:54 +0530

    Update order model

--- base/models.py
+++ base/models.py
@@ -100,7 +100,7 @@ class OrderItem(models.Model):
     qty = models.IntegerField(null=True, blank=True, default=0)
     price = models.DecimalField(
         max_digits=7, decimal_places=2, null=True, blank=True)
-    image = models.CharField(max_length=200, null=True, blank=True)
+    image = models.TextField(null=True, blank=True)
     _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
